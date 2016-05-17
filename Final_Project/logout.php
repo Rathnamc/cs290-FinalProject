4c42406905817ceca8e00998fd7efa73af2192de
Christopher Rathnam <Christopher Rathnam>
2016-05-17 00:23:35 -0400

    Cleaning files

--- Final_Project/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	session_destroy();
-	header('Location: http://www.chrisrathnam.com/Final_Project/ ');
-
-?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	session_destroy();
-	header('Location:http://web.engr.oregonstate.edu/~rathnamc/Final_Project/ ');
-
-?>
